9b17e503c1f612d20786193333c4a8f386f6523f
Saulo Alexandre de Barros <sab2@WINDOWS.CIN.UFPE.BR>
2019-03-29 12:35:48 -0300

    Semana 2 - Aula 2 Modificação

--- PLC_2019.1/Semana2/02_26_aula3.hs
+++ PLC_2019.1/Semana2/02_26_aula3.hs
@@ -19,6 +19,58 @@ nome :: Pessoa -> Nome
 nome pessoa = fst pessoa
 
 {-
-nome ("Saulo", 20)
+nome ("Saulo", 20)  
 -> "Saulo"
 -}
+
+type ParPoli t =  (t,t)
+type ListaParPoli t = [ParPoli t]
+
+-- Casamento de padrao
+
+myNot :: Bool -> Bool
+myNot False = True
+myNot True = False
+
+myOr1 :: Bool -> Bool -> Bool
+myOr1 True True = True
+myOr1 True False = True
+myOr1 False True = True
+myOr1 False False = False
+
+myOr2 :: Bool -> Bool -> Bool
+myOr2 True x = True
+myOr2 False x = x
+
+myOr3 :: Bool -> Bool -> Bool
+myOr3 True _ = True
+myOr3 False x = x
+
+-- Fazendo a Or com guardas
+myOr4 :: Bool -> Bool -> Bool
+myOr4 x y
+ |(x == False) && (y== False) = False
+ |otherwise = True
+
+-- And
+myAnd1 :: Bool -> Bool -> Bool
+myAnd1 True True = True
+myAnd1 True False = False
+myAnd1 False True = False
+myAnd1 False False = False
+
+-- ERRO: myAnd2 True False
+myAnd2 :: Bool -> Bool -> Bool
+myAnd2 True True = True
+myAnd2 False _ = False
+
+-- Fazendo a And com guardas
+myAnd3 :: Bool -> Bool -> Bool
+myAnd3 x y
+ |(x == True) && (y== True) = True
+ |otherwise = False
+
+fatorial :: Int -> Int
+fatorial x
+ | x == 0    = 1                      -- caso base
+ | otherwise =  x * fatorial (x-1)    -- caso recursivo
