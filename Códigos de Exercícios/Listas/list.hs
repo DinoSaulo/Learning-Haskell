a63231a17e5a0b687e801c40e53a09ed8f897627
unknown <sab2@g4c21.windows.cin.ufpe.br>
2019-04-09 08:16:50 -0300

    Arquivos de provas anteriores adicionadas

--- Códigos de Exercícios/Listas/list.hs
+++ Códigos de Exercícios/Listas/list.hs
@@ -1,14 +1,14 @@
 --Dale
 
-QUESTÕES DOS SLIDES (NÃO SEI SE É EXATAMENTE ASSIM QUE ERA PRA FAZER)
+--QUESTï¿½ES DOS SLIDES (Nï¿½O SEI SE ï¿½ EXATAMENTE ASSIM QUE ERA PRA FAZER)
 
 --LISTA 1
 
---Questao 1: está  correto.
+--Questao 1: estï¿½  correto.
 --map :: (a->b) -> [a] -> [b]
 --(.) :: (c->d) -> (e->c) -> (e->d)
 --map . (.)
---para map queremos saber quem é 'a' e quem é 'b'
+--para map queremos saber quem ï¿½ 'a' e quem ï¿½ 'b'
 --vamos usar (c->d) como i e ((e->c) -> (e->d)) como j
 --dessa forma, nossa partida inicial seria i, (c->d).
 --logo, para map (e->c) seria a e (e->d) seria b
@@ -28,7 +28,7 @@ poli a b c = (\x -> a*(x*x) + b*x + c )
 
 --b
 listaPoli :: [(Int, Int, Int)] -> [Int->Int]
-listaPoli l = [poli a b c | (a,b,c) <- l]--USANDO COMPREENSÃO DE LISTA
+listaPoli l = [poli a b c | (a,b,c) <- l]--USANDO COMPREENSï¿½O DE LISTA
 
 listaPoliNormal :: [(Int, Int, Int)] -> [Int->Int]
 listaPoliNormal [] = []
@@ -36,7 +36,7 @@ listaPoliNormal ((a,b,c):xs) = (poli a b c) : listaPoli xs--FAZENDO NORMAL
 
 --c
 appListaPoli :: [Int->Int] -> [Int] -> [Int]
-appListaPoli lp lint = [p i | p <- lp, i <- lint]--USANDO COMPREENSÃO DE LISTA
+appListaPoli lp lint = [p i | p <- lp, i <- lint]--USANDO COMPREENSï¿½O DE LISTA
 
 appListaPoliNormal :: [Int->Int] -> [Int] -> [Int]
 appListaPoliNormal [] _ = []
@@ -47,16 +47,16 @@ appListaPoliNormal (x:xs) (a:b) = (x a):(appListaPoliNormal xs b)--FAZENDO NORMA
 -------------------- Questao 4, letra A --------------------
 
 -- Percorre toda a lista e cria uma nova
--- A nova lista em o mesmo número de elementos que a recebida
--- Porém cada elemento é o número 1
--- Então se aplica a função 'sum' é o número retornado é o tamanho da lista
+-- A nova lista em o mesmo nï¿½mero de elementos que a recebida
+-- Porï¿½m cada elemento ï¿½ o nï¿½mero 1
+-- Entï¿½o se aplica a funï¿½ï¿½o 'sum' ï¿½ o nï¿½mero retornado ï¿½ o tamanho da lista
 --
 functionSizeList list = sum [1 | _ <- list]
 
 -- Compara a linha atual da matrix, x, com a seguinde, (head xs)
--- Se o tamanho for o mesmo então compara a seguinde com a depois desta e assim sucessivamente
--- Se todas tiverem o mesmo tamanho então True é retornado ao chegar no caso "isMatrix [x] = True"
--- Se forem diferentes então entra no "otherwise = False"
+-- Se o tamanho for o mesmo entï¿½o compara a seguinde com a depois desta e assim sucessivamente
+-- Se todas tiverem o mesmo tamanho entï¿½o True ï¿½ retornado ao chegar no caso "isMatrix [x] = True"
+-- Se forem diferentes entï¿½o entra no "otherwise = False"
 --
 isMatrix :: [[t]] -> Bool
 isMatrix [[]] = True
@@ -67,7 +67,7 @@ isMatrix (x:xs)
 
 -- Uma outra forma muito menos eficiente
 -- Cria uma lista com o tamanho de todos os arrays presentes na matriz
--- verifica se todos são iguais
+-- verifica se todos sï¿½o iguais
 tamLista :: [t] -> Int
 tamLista [] = 0
 tamLista (x:xs) = 1 + tamLista xs
@@ -90,16 +90,16 @@ lengthMatrix (x) = allEqualList (lengthSubList x)
 -------------------- Questao 4, letra B --------------------
 
 -- Percorre todas as linhas da matrix e retorna a linha "n"
--- Isso é feito chamando a função removendo a 1 linha da matrix recebida a cada chamada da função
--- Quando a função for chamada "n" vezes "n" será "0" e ela entra no caso "otherwise = x", retornando a linha
+-- Isso ï¿½ feito chamando a funï¿½ï¿½o removendo a 1 linha da matrix recebida a cada chamada da funï¿½ï¿½o
+-- Quando a funï¿½ï¿½o for chamada "n" vezes "n" serï¿½ "0" e ela entra no caso "otherwise = x", retornando a linha
 --
 functionGetLineMatrix :: [[t]] -> Int -> [t]
 functionGetLineMatrix (x:xs) n
 | (n /= 0) = functionGetLineMatrix xs (n-1)
 | otherwise = x
 
--- Mesmo esquema da função "functionGetLineMatrix"
--- Ao chegar na linha n ele substitui o que estava lá pela linha "l"
+-- Mesmo esquema da funï¿½ï¿½o "functionGetLineMatrix"
+-- Ao chegar na linha n ele substitui o que estava lï¿½ pela linha "l"
 --
 functionSwitchLineMatrix :: [[t]] -> [t] -> Int -> [[t]]
 functionSwitchLineMatrix (x:xs) l n
@@ -107,12 +107,12 @@ functionSwitchLineMatrix (x:xs) l n
 | otherwise = [l]++xs
 
 -- Basicamente pegamos a linha "x" e colocamos na linha "y"
---                          [----------------Isso é feito nessa parte----------------]
+--                          [----------------Isso ï¿½ feito nessa parte----------------]
 -- functionSwitchLineMatrix (functionSwitchLineMatrix l (functionGetLineMatrix l x) y) (functionGetLineMatrix l y) x
--- Isso retorna uma matrix, ai chamamos a função novamente passando essa matrix e a linha "y" para ser colocada na linha "x"
--- Porém note que a linha da posição 'y' já foi modificada nessa nova matrix, por isso pegamos a linha "y" da matrix original que nos foi dada, a "l"
+-- Isso retorna uma matrix, ai chamamos a funï¿½ï¿½o novamente passando essa matrix e a linha "y" para ser colocada na linha "x"
+-- Porï¿½m note que a linha da posiï¿½ï¿½o 'y' jï¿½ foi modificada nessa nova matrix, por isso pegamos a linha "y" da matrix original que nos foi dada, a "l"
 --
--- Obs.: Usamos a matrix original apenas para pegar a linha "y" na segunda etapa, porém passamos a já modificada para a função de troca, "functionSwitchLineMatrix"
+-- Obs.: Usamos a matrix original apenas para pegar a linha "y" na segunda etapa, porï¿½m passamos a jï¿½ modificada para a funï¿½ï¿½o de troca, "functionSwitchLineMatrix"
 --
 switchLineMatrix :: [[t]] -> Int -> Int -> [[t]]
 switchLineMatrix l x y = functionSwitchLineMatrix (functionSwitchLineMatrix l (functionGetLineMatrix l x) y) (functionGetLineMatrix l y) x
@@ -120,7 +120,7 @@ switchLineMatrix l x y = functionSwitchLineMatrix (functionSwitchLineMatrix l (f
 
 -------------------- Questao 5 --------------------
 
--- Retorna todas as sublistas cuja soma dos elementos impares é maior que a dos pares
+-- Retorna todas as sublistas cuja soma dos elementos impares ï¿½ maior que a dos pares
 --
 filtrarList :: [[Int]] -> [[Int]]
 filtrarList [[]] = [[]]
@@ -134,7 +134,7 @@ where
 
 -- Filtra as listas e
 --      Filtra as lista novamente:                                              filtrarList x
---      Soma todos os elementos de cada lista já filtrada:                      map sum (filtrarList x)
+--      Soma todos os elementos de cada lista jï¿½ filtrada:                      map sum (filtrarList x)
 --      Pega o valor maximo da lista retornada e multiplica pelo valor "i":     (maximum (map sum (filtrarList x))) * i
 --
 filtrarEInserir :: [[Int]] -> Int -> ([[Int]], Int)
@@ -151,9 +151,9 @@ altMap f g (x:xs:xt) = [f x] ++ [(g xs)] ++ altMap f g xt
 
 -------------------- Questao 6, outro jeito de fazer --------------------
 
--- Recebe duas funções e aplica a primeira sempre ao elemento da cabeça da lista
--- Troca a 1 pela 2 ao chamar a função de forma recursiva
--- Isso é uma gambiarra sem vergonha pra resolver o problema... ( * ´ v ` )_?
+-- Recebe duas funï¿½ï¿½es e aplica a primeira sempre ao elemento da cabeï¿½a da lista
+-- Troca a 1 pela 2 ao chamar a funï¿½ï¿½o de forma recursiva
+-- Isso ï¿½ uma gambiarra sem vergonha pra resolver o problema... ( * ï¿½ v ` )_?
 --
 altMap :: (a -> b) -> (a -> b) -> [a] -> [b]
 altMap _ _ [] = []
@@ -181,9 +181,9 @@ instance Eq Voto where
 -------------------- Questao 7, letra B --------------------
 
 -- Percorre toda a lista e cria uma nova
--- A nova lista em o mesmo número de elementos que a recebida
--- Porém cada elemento é o número 1
--- Então se aplica a função 'sum' é o número retornado é o tamanho da lista
+-- A nova lista em o mesmo nï¿½mero de elementos que a recebida
+-- Porï¿½m cada elemento ï¿½ o nï¿½mero 1
+-- Entï¿½o se aplica a funï¿½ï¿½o 'sum' ï¿½ o nï¿½mero retornado ï¿½ o tamanho da lista
 --
 totalVotos :: Urna -> Voto -> Int
 totalVotos urna voto = sum [1 | x <- urna, x == voto]
@@ -192,7 +192,7 @@ totalVotos urna voto = sum [1 | x <- urna, x == voto]
 
 -- Calcula o total de votos recebidos pra um candidado
 -- Remove todas as entradas desse candidado da urna
--- Chama a função novamente para calcular a apuração do próximo candidato
+-- Chama a funï¿½ï¿½o novamente para calcular a apuraï¿½ï¿½o do prï¿½ximo candidato
 --
 apurar :: Urna -> Apuracao
 apurar [] = []
@@ -218,39 +218,39 @@ f (:xs)
 
 -------------------- Questao 1, letra B --------------------
 
--- Criamos uma nova lista com a função "zip" onde juntamos o "init" da lista original com o "tail" da lista original
+-- Criamos uma nova lista com a funï¿½ï¿½o "zip" onde juntamos o "init" da lista original com o "tail" da lista original
 -- Criando uma lista de tuplas, onde
 --      o elemento "x" de cada tupla vem do "init" da lista original
 --      o elemento "y" de cada tupla vem do "tail" da lista original
 --
--- OBS.: Init se refere a toda a lista, exceto o último elemento
---       O tail é a cauda da lista, o equivalente ao 'ls' na questão
+-- OBS.: Init se refere a toda a lista, exceto o ï¿½ltimo elemento
+--       O tail ï¿½ a cauda da lista, o equivalente ao 'ls' na questï¿½o
 --
 -- OBS2.: O zip funciona pegando os primeiros elementos de cada lista e colocando em uma tupla, depois os segundos elementos e etc
--- Se uma lista for maior que a outra, os elementos que sobrarem SÃO DESCARTADOS
---      Isso acontece na questão, por isso usei o termo "init" para me referir a primeira lista, já que o último elemento vai ser descartado da 1 lista
+-- Se uma lista for maior que a outra, os elementos que sobrarem Sï¿½O DESCARTADOS
+--      Isso acontece na questï¿½o, por isso usei o termo "init" para me referir a primeira lista, jï¿½ que o ï¿½ltimo elemento vai ser descartado da 1 lista
 --
 fCL (l:ls) = [x | (x,y) <- (zip (l:ls) ls), x == y]
 
--------------------- Curiosidades sobre as funções --------------------
+-------------------- Curiosidades sobre as funï¿½ï¿½es --------------------
 
--- A função "zip" pega os elementos de duas listas em cria uma nova lista com estes elementos em uma tupla
+-- A funï¿½ï¿½o "zip" pega os elementos de duas listas em cria uma nova lista com estes elementos em uma tupla
 -- Exemplo:
 --      listaA = [1,2,3,4,5,6,7] = [1..7]
 --      listaB = ['A', 'B', 'C', 'D', 'E'] = ['A'..'E']
 --
 --      zip listaA listaB = [(1,'A'), (2,'B'), (3,'C'), (4,'D'), (5,'E')]
 --
--- Os elementos que sobram são descartados, já que não tem como forma uma tupla
+-- Os elementos que sobram sï¿½o descartados, jï¿½ que nï¿½o tem como forma uma tupla
 --
 ___________________________________________________________
 -------------------- Questao 2 --------------------
 
--- É aplicada inicialmente uma função de filtro para filtrar apenas os elementos maiores ou iguais a "0"
+-- ï¿½ aplicada inicialmente uma funï¿½ï¿½o de filtro para filtrar apenas os elementos maiores ou iguais a "0"
 -- Da lista resultante se filtra todos os elementos menores ou iguais a 100
--- Então se aplica a todos os elementos da lista a função "even" (Se par = True, senão = False)
+-- Entï¿½o se aplica a todos os elementos da lista a funï¿½ï¿½o "even" (Se par = True, senï¿½o = False)
 -- Com isso nos temos uma lista de booleanos
--- Ao aplicar "foldr" passando "True" como argumento com a função de igualdade "=="
+-- Ao aplicar "foldr" passando "True" como argumento com a funï¿½ï¿½o de igualdade "=="
 --      Retorna "True" se todos os argumentos forem "True"
 --      Retorna "False" caso algum deles seja "False"
 --
@@ -258,23 +258,23 @@ g :: [Int] -> Bool
 g [] = True
 g l = foldr (==) True (map even (filter (<= 100) (filter (>=0) l)))
 
--------------------- Curiosidades sobre as funções --------------------
+-------------------- Curiosidades sobre as funï¿½ï¿½es --------------------
 
--- A função filter:
--- Aplica uma função a uma LISTA como um todo e seleciona os ELEMENTOS que satisfazem a CONDIÇÃO
+-- A funï¿½ï¿½o filter:
+-- Aplica uma funï¿½ï¿½o a uma LISTA como um todo e seleciona os ELEMENTOS que satisfazem a CONDIï¿½ï¿½O
 --
 -- filter :: (a -> Bool) -> [a] -> [a]
 --
 
--- A função map:
--- Aplica uma função a CADA ELEMENTO de uma lista e retorna uma lista apos a aplicação da função em cada elemento
+-- A funï¿½ï¿½o map:
+-- Aplica uma funï¿½ï¿½o a CADA ELEMENTO de uma lista e retorna uma lista apos a aplicaï¿½ï¿½o da funï¿½ï¿½o em cada elemento
 --
 -- map :: (a -> b) -> [a] -> [b]que 
 --
 
--- A função foldr:
--- Aplica uma função e um argumento ao ÚLTIMO ELEMENTO de uma lista
--- O resultado é passado como se fosse o novo argumento e a função é aplicada ao PENULTIMO ELEMENTO da lista e assim sucessivamente
+-- A funï¿½ï¿½o foldr:
+-- Aplica uma funï¿½ï¿½o e um argumento ao ï¿½LTIMO ELEMENTO de uma lista
+-- O resultado ï¿½ passado como se fosse o novo argumento e a funï¿½ï¿½o ï¿½ aplicada ao PENULTIMO ELEMENTO da lista e assim sucessivamente
 --
 -- foldr :: Foldable t => (a -> b -> b) -> b -> t a -> b
 --
@@ -286,7 +286,7 @@ type Potencia = Int
 
 data Lampada = Incandescente Fabricante Potencia | Compacta Fabricante Potencia
 
--- Usando as definições de instacias padrão (Não é isso que o prof. quer):
+-- Usando as definiï¿½ï¿½es de instacias padrï¿½o (Nï¿½o ï¿½ isso que o prof. quer):
 -- data Lampada = Incandescente Fabricante Potencia | Compacta Fabricante Potencia deriving (Eq, Show)
 
 -------------------- Questao 3, letra B --------------------
